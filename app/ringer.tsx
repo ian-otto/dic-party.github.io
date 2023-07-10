0124fb17b3f16805b4940b03215008ea5b662586
Chance Nelson <git@chancen.xyz>
2023-07-10 11:10:15 -0700

    add ring functionality

--- app/ringer.tsx
+++ app/ringer.tsx
@@ -5,25 +5,42 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBell, faSkull } from "@fortawesome/free-solid-svg-icons";
+import { Text, Spacer, Card, Button, Loading } from '@nextui-org/react'
 
 library.add(faBell)
 library.add(faSkull)
 
 export interface RingerProps {
-  onSubmit: (string) => void
+  onSubmit: () => void
   loading: boolean
+  disabled: boolean
   banned: boolean
+  userInfo: any
 }
 
 export default function Ringer(props: RingerProps) {
-  if(props.banned) {
-    return (
-      <FontAwesomeIcon icon={faSkull} />
-    );
+  let buttonContent = null
+  if(props.loading) {
+    buttonContent = (
+      <Loading color="currentColor" size="sm" />
+    )
+  } else {
+    buttonContent = "Ding Dong"
   }
+
   return (
-    <>
-      <FontAwesomeIcon icon={faBell} shake size="2xl" />
-    </>
+    <div className="flex flex-col gap-4">
+      <div>
+          <Text color="primary">username> {props.userInfo.username}</Text>
+          <Text 
+            color={props.userInfo.banned ? "error" : "primary"}
+          >
+            status> {props.userInfo.banned ? "banned" : "active"}
+          </Text>
+      </div>
+      <Button bordered size="xl" disabled={props.disabled ? true : false} onPress={props.onSubmit}>
+        {buttonContent}
+      </Button>
+    </div>
   );
 }
