b17515c03a3275845483151cc3fcb2175e7df3aa
Chance Nelson <git@chancen.xyz>
2023-07-12 21:51:29 -0700

    fix asset path issues with gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: '/dic-ring',
+  assetPrefix: '/dic-ring/'
 }
 
 module.exports = nextConfig
