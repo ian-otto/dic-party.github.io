b60363ca19cdde3056d7c8449b922f68223ce8d9
Chance Nelson <git@chancen.xyz>
2023-07-13 00:44:01 -0700

    autospawn keyboard on login

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from 'next/image'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export interface LoginProps {
   onSubmit(password: string): void
@@ -9,19 +9,23 @@ export interface LoginProps {
 
 export default function Login(props: LoginProps) {
   const [password, setPassword] = useState<string>("")
+  const inputRef = useRef()
 
   const submitPassword = (e: any) => {
     e.preventDefault()
     props.onSubmit(password)
   }
 
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
 
   return (
     <>
       <h1 className="font-serif text-peach text-4xl">Welcome back, friend.</h1>
       <p className="font-serif text-lg">Enter your password to access the DiC party suite doorbell.</p>
       <form className="flex flex-col gap-4">
-        <input type="password" autoFocus onChange={e => (setPassword(e.target.value))} placeholder="You remember it, right?" className="bg-transparent border-0 border-b-2 border-b-periwinkle focus:border-b-mint !outline-none font-sans pb-1" />
+        <input type="password" ref={inputRef} onChange={e => (setPassword(e.target.value))} placeholder="You remember it, right?" className="bg-transparent border-0 border-b-2 border-b-periwinkle focus:border-b-mint !outline-none font-sans pb-1" />
         <input type="submit" value="" onClick={e => submitPassword(e)} />
       </form>
     </>
