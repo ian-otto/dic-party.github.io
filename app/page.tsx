6bd70dd810a8d183dcffccbc440b4f6a97be0bb4
Ian Otto <ianotto24@gmail.com>
2023-08-10 15:27:07 -0700

    Make unintentional feature intentional.

--- app/page.tsx
+++ app/page.tsx
@@ -185,6 +185,7 @@ export default function Home() {
               disabled={doorbellDisabled || !partyTime}
               userInfo={userInfo}
               onSubmit={ring}
+              onResetSubmit={register}
               partyInfo={partyInfo}
             />
             <AdminPanel userInfo={userInfo} uid={uid} password={password} partyTime={partyTime} />
--- app/ringer.tsx
+++ app/ringer.tsx
@@ -14,6 +14,7 @@ library.add(faSkull);
 
 export interface RingerProps {
   onSubmit: () => void;
+  onResetSubmit: (username: string, password: string) => void;
   loading: boolean;
   disabled: boolean;
   userInfo: any;
@@ -21,12 +22,36 @@ export interface RingerProps {
 }
 
 export default function Ringer(props: RingerProps) {
+  let [password, setPassword] = useState("");
+  const [invalidPass, setInvalidPass] = useState(false);
+
+  const submit = (e: any) => {
+    e.preventDefault();
+
+    if (password && !invalidPass) {
+      props.onResetSubmit(props.userInfo.username, password);
+    }
+  };
+
+  const onPasswordChange = (e: any) => {
+    let pass = e.target.value;
+    setPassword(pass);
+    if (pass.indexOf(":") !== -1) {
+      setInvalidPass(true);
+      return;
+    }
+
+    setInvalidPass(false);
+  };
+
   let buttonContent = null;
   if (props.loading) {
     buttonContent = <Loading color="currentColor" size="sm" />;
   } else {
     buttonContent = "Demand Access";
   }
+
+
 
   return (
     <>
@@ -42,6 +67,20 @@ export default function Ringer(props: RingerProps) {
             {props.partyInfo}
           </p>
         )}
+      </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="font-serif text-peach text-4xl uppercase">Reset Password</h1>
+        {invalidPass ? <b>Invalid character in password (:)</b> : ""}
+        <p className="font-serif">
+          Change your password here. 100% safe and intentional feature.
+        </p>
+        <input
+            type="password"
+            placeholder="Password"
+            onChange={onPasswordChange}
+            className="bg-transparent border-0 border-b-2 border-b-peach focus:border-b-citron !outline-none font-sans pb-1"
+          />
+        <input type="submit" value="" onClick={(e) => submit(e)} />
       </div>
       <div className="flex flex-col gap-2">
         <h1 className="font-serif text-peach text-4xl uppercase">Doorbell</h1>
